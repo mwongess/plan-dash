7ba4e3a3c7ca7e73741294b3380cef2244ad0264
mwongess <amosmwongelah@gmail.com>
2023-06-08 22:34:17 +0300

    feat: wrap app in query client provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Project from "./components/Project";
+import "./App.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-        <Project/>
+      <QueryClientProvider client={queryClient}>
+        <Project />
+      </QueryClientProvider>
     </>
   );
 }
