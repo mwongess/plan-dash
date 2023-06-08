cf30c9508703d313b684d8b1b18d10106ed02214
mwongess <amosmwongelah@gmail.com>
2023-06-08 12:44:53 +0300

    update context

--- client/src/contexts/PlanDashContext.tsx
+++ client/src/contexts/PlanDashContext.tsx
@@ -1,52 +1,21 @@
-import { IProject,IProjectsContextType } from "../types/projectTypes";
+import {
+  deleteProject,
+  newProject,
+  updateProject,
+} from "../actions/project.actions";
+import { IProject, IProjectsContextType } from "../types/project.types";
 import React, { ReactNode, createContext, useState } from "react";
-import dotenv from "dotenv";
-dotenv.config();
-
-const URL = process.env.BASE_URL!; //Backend API
-
 
 // Custom Hook to access all context stuff
 export const PlanDashContext = createContext<IProjectsContextType | undefined>(
   undefined
-); 
+);
 
 // Provider
 export const PlanDashProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [projects, setProjects] = useState<IProject[]>([]);
-
-  // Create new project
-  const newProject = async (project: IProject) => {
-    try {
-      const res = await fetch(`${URL}` + "/new", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(project),
-      });
-      const data = await res.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  // Update existing project
-  const updateProject = (id: string | number) => { };
-  
-  // Delete a project
-  const deleteProject = async (id: string | number) => {
-    try {
-      const res = await fetch(`${URL + id}`, { method: "DELETE" });
-      const data = await res.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   return (
     <PlanDashContext.Provider
