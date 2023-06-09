8dccbebabfc76b53ea0c84323fa227b2f5f0fb84
mwongess <amosmwongelah@gmail.com>
2023-06-09 11:26:36 +0300

    feat: configure sql server

---
+++ backend/src/config/sqlConfig.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const sqlConfig = {
+    user: process.env.USER as string,
+    password: process.env.PASSWORD as string,
+    server: process.env.SERVER as string,
+    database: 'plandash',
+    options: {
+        encrypt: true,
+        trustServerCertificate: false
+    }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
-import  express, { Application } from 'express'
+import  express, { Application,json } from 'express'
 import { startServer } from './lib/boot'
 
 const app:Application = express()
+app.use(json())
 
 startServer(app)
