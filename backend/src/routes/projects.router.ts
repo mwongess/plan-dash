cc9dee8330f3e81362d2d6f5eebaba73b1adbc1f
mwongess <amosmwongelah@gmail.com>
2023-06-17 22:20:11 +0300

    chore: update project_id

--- backend/src/routes/projects.router.ts
+++ backend/src/routes/projects.router.ts
@@ -1,4 +1,4 @@
-import { DeleteProject, GetProject, GetProjects, NewProject } from "../controllers/project.controllers";
+import { AssignProject, DeleteProject, GetProject, GetProjects, NewProject } from "../controllers/project.controllers";
 import { VerifyToken } from "../middlewares/verifytoken";
 import { Router } from "express";
 
@@ -6,5 +6,6 @@ export const projectsRouter = Router()
 
 projectsRouter.get('/',GetProjects)
 projectsRouter.post('/', NewProject)
-projectsRouter.get('/:id', GetProject)
-projectsRouter.delete('/:id', DeleteProject)
+projectsRouter.get('/:project_id', GetProject)
+projectsRouter.delete('/:project_id', DeleteProject)
+projectsRouter.patch("/:project_id/assign/to/:user_id", AssignProject)
