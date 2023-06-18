e00484a32606964bf2d9d5821d2b0059465208f1
mwongess <amosmwongelah@gmail.com>
2023-06-18 23:20:54 +0300

    chore: fix cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,10 @@ import { projectsRouter } from './routes/projects.router'
 import  express, { Application,json } from 'express'
 import { userRouter } from './routes/user.routes'
 import { startServer } from './lib/boot'
+import cors from 'cors'
 
 const app:Application = express()
+app.use(cors())
 app.use(json())
 
 
