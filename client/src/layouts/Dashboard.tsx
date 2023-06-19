699946504aa4dafe68ea97ec11852caf85f8d433
mwongess <amosmwongelah@gmail.com>
2023-06-19 10:27:50 +0300

    chore: update ui

--- client/src/layouts/Dashboard.tsx
+++ client/src/layouts/Dashboard.tsx
@@ -15,10 +15,10 @@ const Dashboard = () => {
   return (
     <>
     <div className="sm:flex justify-between min-h-screen bg-[#0f172a] text-white p-4 ">
-      <div className="md:w-1/5  border-r border-[#393649]">
+      <div className="md:w-[17%] border-r border-[#393649]">
         <SideNav />
       </div>
-      <div className="md:w-4/5 px-4 ">
+      <div className="md:w-[83%] px-4 ">
         <MainSection/>
       </div>
     </div>
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -2,29 +2,13 @@ import { redirect } from "react-router-dom";
 const Landing = () => {
   return (
     <>
-      <div className="relative">
-        <div className="bg-gray-300 h-64">
-          {/* Content of the background div */}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas magni
-          cupiditate, quod optio dicta incidunt rem quis sunt, saepe ex mollitia
-          quidem, laboriosam perspiciatis. Ipsa ex dicta laudantium eligendi
-          quos!
-        </div>
-        <div className="absolute top-0 left-0 bg-red-500 w-[fit-content] h-16">
-          {/* Content of the overlay div */}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-          quasi non ullam repellendus cupiditate possimus autem explicabo
-          assumenda quam ad ratione soluta, numquam odio modi magnam aliquam
-          sapiente facere reiciendis!
-        </div>
-      </div>
+      
     </>
   );
 };
 
 export const loader = async () => {
-  // return redirect("/dashboard");
-  return "";
+  return redirect("/dashboard");
 };
 
 export default Landing;
